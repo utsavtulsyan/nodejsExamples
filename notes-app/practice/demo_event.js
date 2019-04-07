0dce9e090017d6ada29a426216bccecb4c2f29a1
utsavtulsyan <utsavtulsyan44@gmail.com>
2019-04-07 22:13:14 +0530

    removed node course files

--- notes-app/practice/demo_event.js
+++
@@ -1,8 +0,0 @@
-var events=require('events');
-var eventEmitter=new events.EventEmitter();
-
-var myEventHandler=function(){
-	console.log('I hear a scream');
-}
-eventEmitter.on('scream',myEventHandler);
-eventEmitter.emit('scream');
